03305bfe5b0ea55b0c3f0ef387cb1c41f0eb98e3
Vennila20 <rajavennila44@gmail.com>
2021-12-11 13:05:28 +0530

    task completed

--- src/org/push/ProjectPush.java
+++ src/org/push/ProjectPush.java
@@ -1,5 +1,15 @@
 package org.push;
 
 public class ProjectPush {
+	
+	private void Code() {
+
+		System.out.println("code");
+	}
+	public static void main(String[] args) {
+		
+		ProjectPush p=new ProjectPush();
+		p.Code();
+	}
 
 }
